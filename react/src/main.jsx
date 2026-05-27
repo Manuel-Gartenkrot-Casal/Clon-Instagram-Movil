6a6175b82514d7889bd8a1a3600b439bfb323f6c
Manuel-Gartenkrot-Casal <manuelgartenkrotcasal@gmail.com>
2026-05-27 12:57:40 -0300

    implementado loader

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
 import Publicaciones from './publicaciones.jsx'
 import BarraLateral from './barraLateral.jsx'
 import Header from './Header.jsx';
@@ -9,7 +9,6 @@ import Stories from "./stories.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
     <Header />
     <Stories />
     <BarraLateral />
--- react/src/publicacion.jsx
+++ react/src/publicacion.jsx
@@ -3,9 +3,12 @@ import api from './services/api';
 import PublicacionDetail from './publicacionDetail.jsx';
 import './publicacion.css';
 import perfilImg from "./images/Perfil.webp";
+import Loader from './loader.jsx';
+
 function Publicacion() {
     const [gatos, setGatos] = useState([]);
     const [gatoSeleccionado, setGatoSeleccionado] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         async function obtenerGatos() {
@@ -13,10 +16,11 @@ function Publicacion() {
                 const response = await api.get(
                     'images/search?limit=12&has_breeds=1'
                 );
-
                 setGatos(response.data);
             } catch (error) {
                 console.error(error);
+            } finally {
+                setLoading(false);
             }
         }
 
@@ -29,41 +33,43 @@ function Publicacion() {
 
     if (gatoSeleccionado) {
         return (
-            <>          <button className="volver" onClick={handleBack}> Volver</button>
-
+            <>
+                <button className="volver" onClick={handleBack}>Volver</button>
                 <PublicacionDetail
                     imagen={gatoSeleccionado.url}
                     breeds={gatoSeleccionado.breeds}
                     id={gatoSeleccionado.id}
-
-                /></>
+                />
+            </>
         );
     }
 
-  return (
-    <>
-    <h1>Trending</h1>
+    if (loading) {
+        return <Loader />;
+    }
 
-    <div className="galeria">
-        {gatos.map((gato) => (
-            <div className="card" key={gato.id}>
-                <img
-                    className="foto"
-                    src={gato.url}
-                    alt="Gatito"
-                />
-                <div class = "footerPost">
-                    <img src={perfilImg} alt="Perfil rinoceronte" />
-                    <button onClick={() => setGatoSeleccionado(gato)} >
-                        Ver detalle
-                    </button>
-                </div>
+    return (
+        <>
+            <h1>Trending</h1>
+            <div className="galeria">
+                {gatos.map((gato) => (
+                    <div className="card" key={gato.id}>
+                        <img
+                            className="foto"
+                            src={gato.url}
+                            alt="Gatito"
+                        />
+                        <div className="footerPost">
+                            <img src={perfilImg} alt="Perfil rinoceronte" />
+                            <button onClick={() => setGatoSeleccionado(gato)}>
+                                Ver detalle
+                            </button>
+                        </div>
+                    </div>
+                ))}
             </div>
-        ))}
-
-    </div>
-    </>
-);
+        </>
+    );
 }
 
 export default Publicacion;
